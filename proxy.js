2d1739358fcbbb6ce3088271ec745a3a0e8c7d20
hdias <diaslinoh@gmail.com>
2020-07-14 16:55:31 -0300

    express

--- proxy.js
+++ proxy.js
@@ -1,32 +1,60 @@
-t = "http://www.superdownloads.com.br";
-var http = require('http');
+var express = require('express');
+const path = require('path');
 var url = require('url');
 var request = require('request');
+const { log } = require('console');
+var app = express();
 
+// respond with "hello world" when a GET request is made to the homepage
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-http.createServer(onRequest).listen(process.env.PORT || 3000);
 
-function onRequest(req, res) {
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.get('/**', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    var queryData = url.parse(req.url, true).query;
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader("Content-Type", "*/*; charset=ISO-8859-1");
+    handleRequest(req, res);
+});
 
-    if (queryData.url) {
-        request(queryData.url, (err, resp, body) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.write("Error: " + JSON.stringify(err));
+
+let tmpPort = process.env.PORT || 3000;
+app.listen(tmpPort, () => {
+    console.log("Server running....");
+});
+
+function handleRequest(req, res) {
+    let tmpUrl = req.url.replace(/^\/+/i, '');
+    if (tmpUrl) {
+        request({
+            url: tmpUrl,
+            headers: {
+                'Content-Type': '*/*; charset=ISO-8859-1'
             }
-            else {
-                res.write(body);
-                // console.log(resp);
-            }
-            res.end();
-        });
+        },
+            (err, resp, body) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    let errorMsg = { message: "Unable to get the data from the url.", errors: err };
+                    res.write(JSON.stringify(errorMsg));
+
+                }
+                else {
+                    cT = resp.headers['content-type'];
+                    res.write(body);
+                }
+                res.end();
+            });
 
     }
     else {
         res.writeHead(400, { 'Content-Type': 'text/plain' });
         res.end("No url found");
     }
+
 }
